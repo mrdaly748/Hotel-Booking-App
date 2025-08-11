a8c1ff56b20597ac210c28f364e119218c25b8d6
Hamed Daly <hameddali48@gmail.com>
2025-08-11 18:21:15 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+"import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/db.js";
@@ -40,7 +40,7 @@ const PORT = process.env.PORT || 3000;
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(PORT, () => console.log(Server is running on port ${PORT}));
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
   } catch (error) {
     console.error("Failed to start server:", error.message);
     process.exit(1);
