2671a9e80c06baea20fd72c16baa017621fd0d8c
Hamed Daly <hameddali48@gmail.com>
2025-08-11 16:33:07 +0100

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,29 +1,14 @@
 import mongoose from "mongoose";
 
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function connectDB() {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    const mongoURI = process.env.MONGODB_URI;
-    if (!mongoURI) throw new Error("Missing MONGODB_URI");
-
-    cached.promise = mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      bufferCommands: false,
-      serverSelectionTimeoutMS: 10000,
-    }).then((mongoose) => mongoose.connection);
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () =>
+      console.log("Database connected successfully")
+    );
+    await mongoose.connect(`${process.env.MONGODB_URI}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
   }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
+};
 
 export default connectDB;
-
